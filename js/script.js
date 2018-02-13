41720a3779c83e5a9449475bc0eed09b07f273ad
richard willie <richardwillie.w@gmail.com>
2018-02-13 14:15:33 +0300

    added README index.html and js file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var saySomething = function(whatToSay) {
+        alert(whatToSay);
+      }
+var add = function(number1, number2) {
+        return number1 + number2;
+      }; saySomething("hi");   
+      add(3, 5);
+       var result = add(3, 5);
+      alert(result);
+ var saySomething = function(whatToSay) {
+        alert(whatToSay);
+      };
+
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+
+      var result = add(3, 5);
+      var outputText = "The sum is " + result + ".";
+      saySomething(outputText);
