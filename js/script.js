9fff9adc210f4f195fc760d6543038bfa1b9b74f
richard willie <richardwillie.w@gmail.com>
2018-02-13 14:28:24 +0300

    modified .js file

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
-var saySomething = function(whatToSay) {
-        alert(whatToSay);
-      }
 var add = function(number1, number2) {
-        return number1 + number2;
-      }; saySomething("hi");
-      add(3, 5);
-       var result = add(3, 5);
-      alert(result);
- var saySomething = function(whatToSay) {
-        alert(whatToSay);
-      };
-
-      var add = function(number1, number2) {
+      return number1 + number2;
+    };
+     var add = function(number1, number2) {
         return number1 + number2;
       };
 
-      var result = add(3, 5);
-      var outputText = "The sum is " + result + ".";
-      saySomething(outputText);
-      var add = function(number1, number2) {
-             return number1 + number2;
-           };
+      alert(add(10,5));
+       var add = function(number1, number2) {
+        return number1 + number2;
+      };
 
-           var number1 = parseInt(prompt("Enter a number:"));
-           var number2 = parseInt(prompt("Enter another number:"));
-           var result = add(number1, number2);
+      var number1 = prompt("Enter a number:");
+
+      alert(number1);
+    var add = function(number1, number2) {
+        return number1 + number2;
            alert(result);
-         
