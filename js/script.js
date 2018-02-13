6e28d45b11e141f63523cab949bb1f7b930c0019
richard willie <richardwillie.w@gmail.com>
2018-02-13 14:56:44 +0300

    cahnge js file

--- js/script.js
+++ js/script.js
@@ -1,27 +1,8 @@
 var add = function(number1, number2) {
-      return number1 + number2;
-    };
-     var add = function(number1, number2) {
-        return number1 + number2;
-      };
+       return number1 + number2;
+     };
 
-      alert(add(10,5));
-       var add = function(number1, number2) {
-        return number1 + number2;
-      };
-
-      var number1 = prompt("Enter a number:");
-
-      alert(number1);
-    var add = function(number1, number2) {
-        return number1 + number2;
-           alert(result);
-
-           var add = function(number1, number2) {
-                  return number1 + number2;
-                };
-
-                var number1 = parseInt(prompt("Enter a number:"));
-                var number2 = parseInt(prompt("Enter another number:"));
-                var result = add(number1, number2);
-                alert(result);
+     var number1 = parseInt(prompt("Enter a number:"));
+     var number2 = parseInt(prompt("Enter another number:"));
+     var result = add(number1, number2);
+     alert(result);
